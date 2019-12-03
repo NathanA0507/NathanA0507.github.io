133dce3ccc0b505b551957a80ad276ffc891841e
NathanA0507 <49221442+NathanA0507@users.noreply.github.com>
2019-12-02 21:56:06 -0700

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Account"},{"p":"<Unnamed>","l":"BankStatement"},{"p":"<Unnamed>","l":"Checking"},{"p":"<Unnamed>","l":"Credit"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"Log"},{"p":"<Unnamed>","l":"Person"},{"p":"<Unnamed>","l":"RunBank"},{"p":"<Unnamed>","l":"Savings"}]
